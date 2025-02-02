4775edbf1c00489485622ad81441eae2b003aac6
SaikiranBP <saikiranbpujari@gmail.com>
2025-02-02 13:16:08 +0530

    updating the lines of code

--- Lecture-1/program2.c
+++ Lecture-1/program2.c
@@ -3,9 +3,9 @@ int main()
 {
     int num_1, num_2, sum;
     printf("Enter the first number: ");
-    scanf("%d", num_1);
-    printf("\nEnter the second number: ");
-    scanf("%d", num_2);
+    scanf("%d", &num_1);
+    printf("Enter the second number: ");
+    scanf("%d", &num_2);
     sum = num_1 + num_2;
     printf("The sum of %d and %d is %d", num_1, num_2, sum);
     return 0;
