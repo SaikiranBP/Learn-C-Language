c5ee6316f6c98b5ea2376daeba9fd177576d5b3e
SaikiranBP <saikiranbpujari@gmail.com>
2025-02-14 18:32:28 +0530

    program to concatenate two strings

---
+++ Chapter-8/program6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+// This program concatenates two strings
+int main()
+{
+    char str1[100], str2[100], str3[100];
+    int i=0, j=0;
+    printf("Enter the first string: ");
+    gets(str1);
+    printf("Enter the second string: ");
+    gets(str2);
+    while(str1[i] != '\0')
+    {
+        str3[j] = str1[i];
+        i++;
+        j++;
+    }
+    i=0;
+    while(str2[i] != '\0')
+    {
+        str3[j] = str2[i];
+        i++;
+        j++;
+    }
+    str3[j] = '\0';
+    printf("The concatenated string is:");
+    puts(str3);
+    return 0;
+}
